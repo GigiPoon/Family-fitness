1553eca3e965eea596375a616b989d9df4a7bdef
Gigi <yuengepoon@gmail.com>
2024-04-04 11:45:52 -0400

    rough draft

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+    './popup/popup.html',
+    './exercise.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        gothic: ["Gothic A1", "sans-serif"],
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'sandy': '#F0E9D8',
+        'redish': '#9B3D3D',
+      },
+      screens: {
+        'sm': '700px',
+        // => @media (max-width: 700px) { ... }
+
+        'md': '960px',
+        // => @media (max-width: 960px) { ... }
+      },
+    },
+  },
+  plugins: [],
+}
